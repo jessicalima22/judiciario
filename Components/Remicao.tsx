08a0672d5dbb0319b0ad7faa00224a8ab8a68f8e
jessicalima22 <jessica.lima22@hotmail.com>
2023-06-08 23:47:11 -0300

    inicial date changes

--- Components/Remicao.tsx
+++ Components/Remicao.tsx
@@ -1,5 +1,28 @@
-function Remicao(){
-    return(
-       <h1>Calcule a remição</h1>
+import moment from "moment";
+
+export default function Remicao(){
+
+    //Padrão brasileiro
+
+    let data = new Date();
+
+    function formatarData(data:Date){
+        let novaData = new Date(data);
+        return `${novaData.getDate()}/${novaData.getMonth()+1}/${novaData.getFullYear()}`
+    }
+
+    var d1 = "29/08/2020";
+    var d2 = "12/02/2020";
+
+    var diff = moment(d2, "DD/MM/YYYY").diff(moment(d1,"DD/MM/YYYY"));
+    var anos:any = moment.duration(diff).years;
+
+
+    return (
+       <div>
+           <h1>Calcule a remição</h1>
+           <p> <input type="date" name="dataAtual" id="dataAtual" /></p>
+           {anos}
+       </div>
     )
 }
---
+++ src/pages/remicao.tsx
@@ -0,0 +1,7 @@
+import Remicao from "../../Components/Remicao";
+
+export default function RemicaoPage (){
+    return(
+        <Remicao/>
+    )
+}
