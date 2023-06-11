36e638c1b7e28645af7729a44fcf5b496cf94d03
jessicalima22 <jessica.lima22@hotmail.com>
2023-06-10 23:33:52 -0300

    inputs ok

--- Components/Remicao.tsx
+++ Components/Remicao.tsx
@@ -1,7 +1,5 @@
 import moment from "moment";
-
-
-
+import { useState } from "react";
 
 
 export default function Remicao(){
@@ -20,14 +18,41 @@ export default function Remicao(){
     const today = hoje
     
 
-    
+    const [typedInicio, setTypedInicio] = useState()
+    const [typedTermino, setTypedTermino] = useState()
+    const [typedARemir, setTypedARemir] = useState(0)
+    const [typedNovoTermino, setTypedNovoTermino] = useState()
 
+    function handleInicio(e){
+       setTypedInicio(e.target.value)
+    }
+    
+    function handleARemir(e){
+        setTypedARemir(e.target.value)
+     }
+     
+     function handleTermino(e){
+        setTypedTermino(e.target.value)
+     }
+
+     function handleNovoTermino(e){
+        setTypedNovoTermino(e.target.value)
+     }
 
     return (
-       <div>
+       <div className={`
+       flex flex-col
+       `}>
            <h1>Calcule a remição</h1>
-           <p> <input type="date" name="dataAtual" id="dataAtual" /></p>
-           {today}
+           <label htmlFor="inicio">Início do cumprimento da pena em:</label>
+           <input type="date" name="inicio"  onChange={handleInicio}/>
+           <label htmlFor="termino">Término previsto para a pena:</label>
+           <input type="date" name="termino" onChange={handleTermino} />
+           <label htmlFor="diasARemir">Quantidade de dias a remir:</label>
+           <input type="number" name="diasARemir" onChange={handleARemir}/>
+           <label htmlFor="novoTermino">Data de término atualizada:</label>
+           <input type="text" name="novoTermino" onChange={handleNovoTermino}/>
+           
        </div>
     )
 }
